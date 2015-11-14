4ca58bcc27d75c7aa75fc7e697d6043c44277eb8
Alex Wang <aw1621107@gmail.com>
2015-11-14 14:51:25 -0500

    Add basic GPA class

---
+++ Hackthon/src/algorithm/GPA.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class GPA {
+	final BigDecimal gpa;
+	final GPARange type;
+
+	public GPA(BigDecimal gpa, GPARange type) {
+		this.gpa = gpa;
+		this.type = type;
+	}
+
+	public enum GPARange {
+		LOWER_THAN,
+		OR_LOWER,
+		GREATER_THAN,
+		AT_LEAST;
+	}
