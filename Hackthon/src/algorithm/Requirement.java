0fcf63c07b106e5046460edc84879f9ad5e3622d
Alex Wang <aw1621107@gmail.com>
2015-11-14 14:56:46 -0500

    Add equals() and hashCode() to Requirement
    These depends on the required thing implementing equals() and hashCode()
    correctly

--- Hackthon/src/algorithm/Requirement.java
+++ Hackthon/src/algorithm/Requirement.java
@@ -11,4 +11,16 @@ public class Requirement<T> {
 
 	public Weight getWeight() { return weight; }
 	public T getRequired() { return required; }
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Requirement)) { return; }
+		Requirement<?> other = (Requirement<?>) o;
+		return weight == other.weight && required.equals(o.required);
+	}
+
+	@Override
+	public int hashCode() {
+		return weight.hashCode() ^ required.hashCode();
+	}
 }
