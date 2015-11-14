f9f8f7b6f5a9d641b9022b5f6e262299324dc7f7
Alex Wang <aw1621107@gmail.com>
2015-11-14 15:07:54 -0500

    Change Major to regular enum
    Now that preference is set somewhere else, no need to have a separate
    class for major type

---
+++ Hackthon/src/algorithm/MajorType.java
@@ -0,0 +1,49 @@
+package algorithm;
+
+import java.util.Set;
+
+public enum MajorType {
+	ACCOUNTING(),
+	AGRIBUSINESS(),
+	AGRICULTURAL_ECONOMICS(),
+	APPLIED_ECONOMICS(),
+	APPLIED_MATHEMATICS(),
+	APPLICED_STATISTICS(),
+	ARTIFICIAL_INTELLIGENCE(),
+	BANKING(),
+	BANKING_LAW(),
+	BUSINESS_ADMINISTRATION(),
+	BUSINESS_ANALYTICS(),
+	CHEMISTRY(),
+	COMMERCIAL_LAW(),
+	COMPUTER_ENGINEERING(),
+	COMPUTER_INFORMATION_SYSTEMS(),
+	COMPUTER_SCIENCE(),
+	ECONOMICS(),
+	ENGINEERING(),
+	FINANCE(),
+	HYBRID_TRADER_TRAINEE(),
+	INFORMATION_SYSTEMS(),
+	INFORMATION_TECHNOLOGY(),
+	MACHINE_LEARNING(),
+	MARKET_RESEARCH(),
+	MARKETING(),
+	MATH(),
+	OPERATIONS_RESEARCH(),
+	PHYSICS(),
+	PSYCHOLOGY(),
+	SOFTWARE_ENGINEER(),
+	STATISTICS(),
+	TRADER_TRAINEE();
+
+	private Set<MajorType> related;
+
+	private MajorType(MajorType related...) {
+		related = EnumSet.noneOf(MajorType.class);
+		for (MajorType type : related) {
+			related.add(type);
+		}
+	}
+	// Need way to categorize "hard science"
+}
+
