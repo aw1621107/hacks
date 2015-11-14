6a32c18b955e0ace8cd08b386c8cf8a66c813944
d3bird <dogbird9@gmail.com>
2015-11-14 14:20:25 -0500

    major sorter

--- Hackthon/src/algorithm/Sorter.java
+++ Hackthon/src/algorithm/Sorter.java
@@ -1,31 +1,33 @@
 package algorithm;
 
 public class Sorter {
+	
+	
 	private static String temp = "";
+
+	private static int good = 0;
+	private static int bad =  0;
 	
-	public static void main(String [] args){
-		if (temp.contentEquals("Economics") || temp.contentEquals("mathematics")|| temp.contentEquals("Finance")){
-			
-		}else if (temp.contentEquals("Analytics")){
-			
-		}else if (temp.contentEquals("Science")){
-			
-		}else if (){
-			
+	public static void main(String[] args) {
+		if (temp.contentEquals("Economics") || temp.contentEquals("mathematics") || temp.contentEquals("Finance")) {
+
+		} else if (temp.contentEquals("Analytics")) {
+
+		} else if (temp.contentEquals("Science")) {
+
+		} else if (temp.contentEquals("Communications")) {
+
+		} else if (temp.contentEquals("Business")) {
+
+		} else if (temp.contentEquals("Science;")) {
+
+		} else if (temp.contentEquals("International")) {
+
 		}
-		
-		
-	/*education 
-	intern
-	develper
-	analyist
-	engineer	
-	*/
+
+		/*
+		 * education intern develper analyist engineer
+		 */
 	}
-	
-	
-	
-	
-	
-	
+
 }
