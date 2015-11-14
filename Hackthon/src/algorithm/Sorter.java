2d8b062240c15e326f14e5dfe20c0b37691ad652
d3bird <dogbird9@gmail.com>
2015-11-14 14:13:29 -0500

    basic sorter = framework for now

---
+++ Hackthon/src/algorithm/Sorter.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+public class Sorter {
+	private static String temp = "";
+	
+	public static void main(String [] args){
+		if (temp.contentEquals("Economics") || temp.contentEquals("mathematics")|| temp.contentEquals("Finance")){
+			
+		}else if (temp.contentEquals("Analytics")){
+			
+		}else if (temp.contentEquals("Science")){
+			
+		}else if (){
+			
+		}
+		
+		
+	/*education 
+	intern
+	develper
+	analyist
+	engineer	
+	*/
+	}
+	
+	
+	
+	
+	
+	
+}
