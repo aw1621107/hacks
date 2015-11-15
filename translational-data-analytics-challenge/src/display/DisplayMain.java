bed038e66bb08a3e6b0a8a8da33b85ecb9fd260e
d3bird <dogbird9@gmail.com>
2015-11-15 02:10:29 -0500

    the gui now shows every example/working on scollba

--- translational-data-analytics-challenge/src/display/DisplayMain.java
+++ translational-data-analytics-challenge/src/display/DisplayMain.java
@@ -1,27 +1,29 @@
 package display;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 
 public class DisplayMain extends JFrame/* implements ActionListener */ {
 
 	private static final long serialVersionUID = 1L;
 
-	public static int tickcount = 5;
-	private static String TITLE = "TITLE";
+	public static int tickcount = 92;
+	private static String TITLE = "results";
 	public static int HEIGHT = 800;
 	public static int WIDTH = 720;
 	private static JFrame frame = new JFrame();
-	private static JFrame frame2 = new JFrame();
-	public static String tempout = "this is a test";
+	public static int percenofkeyords =7;
 
 	public static int bwidth = 170;
 	public static int bheight = 50;
@@ -30,10 +32,18 @@ public class DisplayMain extends JFrame/* implements ActionListener */ {
 	// private static JButton button2 = new JButton("5 (LINE_END)");
 
 	// private JButton button = new JButton();
-
+private static Container pane = frame.getContentPane();
+private static		JScrollPane  panil = new JScrollPane ();
 	public static void main(String[] args) {
 
-		frame.setLayout(new FlowLayout());
+		
+		
+		
+		
+		
+		JScrollBar vbar = new JScrollBar(JScrollBar.VERTICAL, 30, 40, 0, 300);
+		
+		//panil.setLayout(new FlowLayout());
 		frame.setSize(HEIGHT, WIDTH);
 		frame.setTitle(TITLE);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -41,11 +51,19 @@ public class DisplayMain extends JFrame/* implements ActionListener */ {
 		frame.setResizable(false);
 		frame.setVisible(true);
 
+		
+		JLabel lable = new JLabel("the percen of applicants who used key words are " + percenofkeyords + "%");
+		lable.setPreferredSize(new Dimension(800, bheight));
+					frame.add(lable, BorderLayout.PAGE_START);
+				frame.add(panil);	
+				
+
+
 		for (int i = tickcount; i <= tickcount && i >= 0; i--) {
-			// Container pane = aFrame.getContentPane()...
+			
 			JButton button = new JButton("Button 1)");
 			button.setPreferredSize(new Dimension(bwidth, bheight));
-			frame.add(button, BorderLayout.PAGE_START);
+			panil.add(button, BorderLayout.PAGE_START);
 			button.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					System.out.println("this is from button one");
@@ -56,7 +74,7 @@ public class DisplayMain extends JFrame/* implements ActionListener */ {
 			// typical usage of BorderLayout.
 			JButton button2 = new JButton("Buttton 2)");
 			button2.setPreferredSize(new Dimension(bwidth, bheight));
-			frame.add(button2, BorderLayout.CENTER);
+			panil.add(button2, BorderLayout.CENTER);
 			button2.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					System.out.println("this is from button two");
@@ -64,7 +82,7 @@ public class DisplayMain extends JFrame/* implements ActionListener */ {
 			});
 
 			JButton button3 = new JButton("Button 3");
-			frame.add(button3, BorderLayout.LINE_START);
+			panil.add(button3, BorderLayout.LINE_START);
 			button3.setPreferredSize(new Dimension(bwidth, bheight));
 			button3.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
@@ -73,7 +91,7 @@ public class DisplayMain extends JFrame/* implements ActionListener */ {
 			});
 
 			JButton button4 = new JButton("Button 4");
-			frame.add(button4, BorderLayout.PAGE_END);
+			panil.add(button4, BorderLayout.PAGE_END);
 			button4.setPreferredSize(new Dimension(bwidth, bheight));
 			button4.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
@@ -82,7 +100,7 @@ public class DisplayMain extends JFrame/* implements ActionListener */ {
 						// Container pane = aFrame.getContentPane()...
 						JButton button = new JButton("Button 1)");
 						button.setPreferredSize(new Dimension(bwidth, bheight));
-						frame.add(button, BorderLayout.PAGE_START);
+						panil.add(button, BorderLayout.PAGE_START);
 						button.addActionListener(new ActionListener() {
 							public void actionPerformed(ActionEvent e) {
 								System.out.println("this is from button one");
@@ -93,7 +111,7 @@ public class DisplayMain extends JFrame/* implements ActionListener */ {
 						// typical usage of BorderLayout.
 						JButton button2 = new JButton("Buttton 2)");
 						button2.setPreferredSize(new Dimension(bwidth, bheight));
-						frame.add(button2, BorderLayout.CENTER);
+						panil.add(button2, BorderLayout.CENTER);
 						button2.addActionListener(new ActionListener() {
 							public void actionPerformed(ActionEvent e) {
 								System.out.println("this is from button two");
@@ -101,7 +119,7 @@ public class DisplayMain extends JFrame/* implements ActionListener */ {
 						});
 
 						JButton button3 = new JButton("Button 3");
-						frame.add(button3, BorderLayout.LINE_START);
+						panil.add(button3, BorderLayout.LINE_START);
 						button3.setPreferredSize(new Dimension(bwidth, bheight));
 						button3.addActionListener(new ActionListener() {
 							public void actionPerformed(ActionEvent e) {
@@ -110,13 +128,13 @@ public class DisplayMain extends JFrame/* implements ActionListener */ {
 						});
 
 						JButton button4 = new JButton("Button 4");
-						frame.add(button4, BorderLayout.PAGE_END);
+						panil.add(button4, BorderLayout.PAGE_END);
 						button4.setPreferredSize(new Dimension(bwidth, bheight));
 						button4.addActionListener(new ActionListener() {
 							public void actionPerformed(ActionEvent e) {
 								System.out.println("this is from button four");
 
-								frame.setVisible(false);
+								
 							};
 						});
 
